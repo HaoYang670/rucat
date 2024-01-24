d914700fd36a854b138c13a7a2d2636b1f1701f1
remzi <13716567376yh@gmail.com>
2024-01-24 15:48:25 +0800

    add deterministic level
    Signed-off-by: remzi <13716567376yh@gmail.com>

---
+++ rucat_job/src/rdd/deterministic_level.rs
@@ -0,0 +1,9 @@
+/// The deterministic level of RDD's output
+enum DeterministicLevel {
+  /// The RDD output is always the same data set in the same order after a rerun.
+  Determinate,
+  /// The RDD output is always the same data set but the order can be different after a rerun.
+  Unordered,
+  /// The RDD output can be different after a rerun.
+  Indeterminate
+}
