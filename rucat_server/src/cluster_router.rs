b0c3185fa54f944aa469322dce112b0d33258574
remzi <13716567376yh@gmail.com>
2024-04-01 14:33:17 +0800

    define data store
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- rucat_server/src/cluster_router.rs
+++ rucat_server/src/cluster_router.rs
@@ -3,10 +3,23 @@ use serde::{Deserialize, Serialize};
 
 enum ClusterState {
     RUNNING,
+    ERROR,
     SLEEPING,
 }
 
-struct ClusterId(u8);
+/// For future
+enum ClusterType {
+    Datafusion,
+    Rucat,
+}
+
+pub(crate) type ClusterId = u8;
+
+pub(crate) struct Cluster<'a> {
+    name: &'a str,
+    id: ClusterId,
+    state: ClusterState,
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct CreateClusterRequest {
@@ -22,19 +35,19 @@ async fn delete_cluster() -> () {
     todo!()
 }
 
-async fn stop_cluster() -> () {
+async fn stop_cluster(id: ClusterId) -> () {
     todo!()
 }
 
-async fn start_cluster() -> () {
+async fn start_cluster(id: ClusterId) -> () {
     todo!()
 }
 
-async fn restart_cluster() -> () {
+async fn restart_cluster(id: ClusterId) -> () {
     todo!()
 }
 
-async fn get_cluster() -> () {
+async fn get_cluster(id: ClusterId) -> () {
     todo!()
 }
 
