0f9c25dcb421b566dbda95a3785d5e684cf16d4d
remzi <13716567376yh@gmail.com>
2024-05-11 23:24:22 +0800

    get cluster test
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- rucat_server/tests/clusters.rs
+++ rucat_server/tests/clusters.rs
@@ -1,6 +1,7 @@
 use axum_test::TestServer;
 use rucat_common::error::Result;
 use rucat_server::get_server;
+use serde_json::json;
 
 /// server with embedded datastore and authentication disabled
 async fn get_test_server() -> Result<TestServer> {
@@ -26,5 +27,37 @@ async fn root_get_request() -> Result<()> {
 
     response.assert_status_ok();
     response.assert_text("welcome to rucat");
+    Ok(())
+}
+
+#[tokio::test]
+async fn get_cluster_not_found() -> Result<()> {
+    let server = get_test_server().await?;
+
+    let response = server.get("/cluster/any").await;
+
+    response.assert_status_not_found();
+    Ok(())
+}
+
+#[tokio::test]
+async fn create_and_get_cluster() -> Result<()> {
+    let server = get_test_server().await?;
+    let response = server
+        .post("/cluster")
+        //.json(r#"{"name": "test","cluster_type": "Ballista"}"#).await;
+        .json(&json!({
+            "name": "test",
+            "cluster_type": "Ballista"
+        }))
+        .await;
+
+    //response.assert_status_ok();
+
+    let cluster_id = response.text();
+    let response = server.get(&format!("/cluster/{}", cluster_id)).await;
+    response.assert_status_ok();
+    response.assert_text(r#"ClusterInfo { name: "test", cluster_type: Ballista, state: Pending }"#);
+
     Ok(())
 }
